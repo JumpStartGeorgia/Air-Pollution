b2772acb71da209f237cd2dba31935d0428bcbde
meripepanashvili <mpepa13@freeuni.edu.ge>
2016-12-12 15:33:37 +0400

    pledge slide buttons update

--- app/assets/javascripts/pledge.js
+++ app/assets/javascripts/pledge.js
@@ -20,8 +20,8 @@ $(document).ready(function(){
 	    {
 	      breakpoint: 5000,
 	      settings: {
-	        slidesToShow: 3,
-	        slidesToScroll: 3,
+	        slidesToShow: 2,
+	        slidesToScroll: 2,
 	        infinite: true,
 	        dots: true
 	      }
