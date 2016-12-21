936db5eb1970f388c0622d912456104411a489cc
meripepanashvili <mpepa13@freeuni.edu.ge>
2016-12-21 14:52:36 +0400

    highlight height change

--- app/assets/javascripts/highlights.js
+++ app/assets/javascripts/highlights.js
@@ -1,17 +1,25 @@
 
 var auto_slideshow;
 var slideshow_time = 5000;
+var higlight_height;
 
 (function() {
 
   $(document).ready(function() {
   	highlight_button_functions();
   	show_first_highlight();
+  	set_highlight_height();
 
   	auto_slideshow  = window.setTimeout(change_highlight, slideshow_time);
 
-  });
+  	$(window).scroll(function() {
+	 	change_highlight_height();
+	});
 
+ 	$(window).resize(function(){
+  		set_highlight_height();
+ 	});
+  });
 
 })();
 
@@ -42,11 +50,8 @@ function change_highlight(index, direction) {
 	var curr_highlight_index = parseInt(curr_highlight.attr("data-index"));
 	var next_highlight_index = 0;
 	curr_highlight.removeClass("active");
-	// $(".highlight-item").hide();
 	$(".highlight-item").stop(true, true); 
-
 	$(".highlight-item-button").removeClass("active");
-
 
 	var manual_change = false;
 	clearTimeout(auto_slideshow);		
@@ -70,13 +75,20 @@ function change_highlight(index, direction) {
 		next_highlight_index = index;
 	}
 
-
 	var next_item = $(".highlights").find('.highlight-item:eq('+ (next_highlight_index) +')');
 	next_item.addClass('active');
-	// next_item.fadeIn(2000);
 
 	$(".highlights").find('.highlight-item-button:eq('+ (next_highlight_index) +')').addClass('active');
 
 	auto_slideshow = window.setTimeout(change_highlight, slideshow_time);
-
 }
+
+function set_highlight_height() {
+	$(".highlights .highlight-image").css('height', '');
+	highlights_height = $(".highlights .highlight-image").height();
+}
+
+function change_highlight_height() {
+	 $(".highlights .highlight-image").css("height",  highlights_height);
+}
+
