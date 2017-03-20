88c7b566ff6864a1b8c6e5556b2567e006f86a96
jason <jason.addie@gmail.com>
2017-03-20 17:38:24 +0400

    reverting deploy profiles

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,27 +1,13 @@
-set :domain, 'alpha.jumpstart.ge'
-set :user, 'filter-staging'
-set :application, 'Filter-Staging'
+set :domain, 'delta.jumpstart.ge'
+set :user, 'filter'
+set :application, 'Filter'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/JumpStartGeorgia/Air-Pollution.git'
 set :branch, 'master'
-set :web_url, 'dev-filter.jumpstart.ge'
-set :visible_to_robots, false
+set :web_url, 'filter.ge'
+set :secondary_web_url, 'www.filter.ge'
 set :use_ssl, true
 set :puma_worker_count, '2'
 set :puma_thread_count_min, '1'
 set :puma_thread_count_max, '8'
 
-
-# set :domain, 'delta.jumpstart.ge'
-# set :user, 'filter'
-# set :application, 'Filter'
-# # easier to use https; if you use ssh then you have to create key on server
-# set :repository, 'https://github.com/JumpStartGeorgia/Air-Pollution.git'
-# set :branch, 'master'
-# set :web_url, 'filter.ge'
-# #set :secondary_web_url, 'www.filter.ge'
-# set :use_ssl, true
-# set :puma_worker_count, '2'
-# set :puma_thread_count_min, '1'
-# set :puma_thread_count_max, '8'
-# set :visible_to_robots, false
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,6 +7,3 @@ set :branch, 'master'
 set :web_url, 'dev-filter.jumpstart.ge'
 set :visible_to_robots, false
 set :use_ssl, true
-set :puma_worker_count, '2'
-set :puma_thread_count_min, '1'
-set :puma_thread_count_max, '8'
