ca25f02946fea5dcf891bf0efc772391fa0aae72
jason <jason.addie@gmail.com>
2017-03-20 16:27:36 +0400

    deploying prod env to staging

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,15 +1,27 @@
-set :domain, 'delta.jumpstart.ge'
-set :user, 'filter'
-set :application, 'Filter'
+set :domain, 'alpha.jumpstart.ge'
+set :user, 'filter-staging'
+set :application, 'Filter-Staging'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/JumpStartGeorgia/Air-Pollution.git'
 set :branch, 'master'
-set :web_url, 'filter.ge'
-#set :secondary_web_url, 'www.filter.ge'
+set :web_url, 'dev-filter.jumpstart.ge'
+set :visible_to_robots, false
 set :use_ssl, true
 set :puma_worker_count, '2'
 set :puma_thread_count_min, '1'
 set :puma_thread_count_max, '8'
 
 
-set :visible_to_robots, false
+# set :domain, 'delta.jumpstart.ge'
+# set :user, 'filter'
+# set :application, 'Filter'
+# # easier to use https; if you use ssh then you have to create key on server
+# set :repository, 'https://github.com/JumpStartGeorgia/Air-Pollution.git'
+# set :branch, 'master'
+# set :web_url, 'filter.ge'
+# #set :secondary_web_url, 'www.filter.ge'
+# set :use_ssl, true
+# set :puma_worker_count, '2'
+# set :puma_thread_count_min, '1'
+# set :puma_thread_count_max, '8'
+# set :visible_to_robots, false
